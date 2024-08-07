3756ce55f8dd4fc111b7ea09f9a4b1cd3f9461ba
Omer Catalbas <78023319+omrctlbs@users.noreply.github.com>
2024-08-07 14:46:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿//// APP - 1 Whether the user votes or not according to the age entered from the keyboard
+
+//Console.WriteLine("Hello, please enter your age: ");
+//int age = Convert.ToInt32(Console.ReadLine());
+//int legalAge = 18;
+//int ageDifference = 18 - age;
+//if  (age >= 18)
+//{
+//    Console.WriteLine("You can vote.");
+//}
+//else
+//{
+//    Console.WriteLine("You cannot vote. You can vote in " + ageDifference + " years.");
+//}
+
+///////////////////////////////////////////////////////////////////
+//// APP - 2 Whether the number entered from the keyboard is positive or negative
+
+//Console.WriteLine("Hello, please enter a number: ");
+//int number = Convert.ToInt32(Console.ReadLine());
+//Console.Clear();
+
+//if (number <= 0)
+//{
+//    Console.WriteLine(number + " is a negative (-) number.");
+//}
+//else
+//{
+//    Console.WriteLine(number + " is a positive (+) number.");
+//}
+
+///////////////////////////////////////////////////////////////////
+//// APP - 3 Finding out whether the entered number is odd or even
+
+//Console.WriteLine("Hello, please enter a number: ");
+//int number = Convert.ToInt32(Console.ReadLine());
+//Console.Clear();
+
+//var result = (number % 2 == 0) ? "The number is even" : "The number is odd";
+
+//Console.WriteLine(result);
